3a3f248af13365be7b2cbf18bbf08368c30a6318
Johnatan <jsalazarg@sura.com.co>
2022-09-27 14:17:33 -0500

    Se crea un formulario DBZ.

--- 01-bases/src/app/app.module.ts
+++ 01-bases/src/app/app.module.ts
@@ -2,19 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador.component';
-import { HeroeComponent } from './heroes/heroe/heroe.component';
-import { ListadoComponent } from './heroes/listado/listado.component';
+
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador/contador.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ContadorComponent,
-    HeroeComponent,
-    ListadoComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ 01-bases/src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html'
+})
+export class MainPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  agregar( ){
+    console.log('Esta es una prueba!!');
+  }
+
+}
